430ce042ab68f47d7c764e1fa8f770ab657a1aba
Qowiyyu Adzkar <qowiyyuadzkar@gmail.com>
2022-06-12 00:28:12 +0700

    update variable fungsiCustom

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
+
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +20,43 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback)=>{
+  const result = [];
+
+  // Data 1
+  fs.readFile(file1, "utf8", (err,data) => {
+    if(err){
+      fnCallback(err,null);
+    }else{
+      const data1 = JSON.parse(data);
+      result.push(data1.message.split(" ")[1]);
+
+      // Data 2
+      fs.readFile(file2, "utf8", (err,data) => {
+        if(err){
+          fnCallback(err,null);
+        }else{
+          
+          const data2 = JSON.parse(data);
+          result.push(data2[0].message.split(" ")[1]);
+
+          // Data 3
+          fs.readFile(file3, "utf8", (err,data) => {
+            if(err){
+              fnCallback(err,null);
+            }else{
+
+              const data3 = JSON.parse(data);
+              result.push(data3[0].data.message.split(" ")[1]);
+              fnCallback(null,result);
+            }
+          });
+        }
+      });
+    }
+  });
+};
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
